efb008bebd2f1a339576ba82354d1c40c79355a9
Sarthak Negi <bme@Sarthaks-MacBook-Pro.local>
2021-06-11 12:27:15 +0530

    Day7

--- 10.Strings/mostFrequentChar.cpp
+++ 10.Strings/mostFrequentChar.cpp
@@ -1,32 +1,32 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
+#include <iostream>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
 int main()
 {
     string s;
-    cin>>s;
+    cin >> s;
 
     int freq[26] = {0};
 
-    for(int i=0; i<26; i++){
+    for (int i = 0; i < 26; i++)
+    {
         freq[s[i] - 'a']++;
     }
 
     int maxFreq = 0;
     char freqChar = 'a';
-     
-    for(int i=0; i<26; i++){
-        if(freq[i]>maxFreq){
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (freq[i] > maxFreq)
+        {
             maxFreq = freq[i];
             freqChar = 'a' + i;
         }
     }
 
-    
-
-    cout<< maxFreq<<" "<<freqChar<<endl;
-    
+    cout << maxFreq << " " << freqChar << endl;
 }
